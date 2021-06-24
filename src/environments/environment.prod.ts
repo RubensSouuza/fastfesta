10ffb7efe6f30bee10ba25495fa93476174c0123
RubensSouuza <80710036+RubensSouuza@users.noreply.github.com>
2021-06-24 11:05:39 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDrKBrcmeJ8dG9usJjMVI55FAjDuhB8TMM",
+    authDomain: "fastfesta-d4eff.firebaseapp.com",
+    projectId: "fastfesta-d4eff",
+    storageBucket: "fastfesta-d4eff.appspot.com",
+    messagingSenderId: "576822880497",
+    appId: "1:576822880497:web:32a32844e75877fe2c6150"
+  }
 };
